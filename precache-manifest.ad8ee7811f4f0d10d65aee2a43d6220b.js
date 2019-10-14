abd983503d5bf1b505ae8ea3b69c12abdfd70c99
Peter Tshiani <tshianipeter@gmail.com>
2019-10-14 12:46:49 +0200

    Updates

---
+++ precache-manifest.ad8ee7811f4f0d10d65aee2a43d6220b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89badc389281da2c35fcb37d6cbf91eb",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "6ed3b1a2e2e3a8f82ffb",
+    "url": "/snake-game/static/css/main.4affd99a.chunk.css"
+  },
+  {
+    "revision": "50193c22c08ec38c7da9",
+    "url": "/snake-game/static/js/2.1798119e.chunk.js"
+  },
+  {
+    "revision": "6ed3b1a2e2e3a8f82ffb",
+    "url": "/snake-game/static/js/main.31f5c69b.chunk.js"
+  },
+  {
+    "revision": "0c050d4293077fde8533",
+    "url": "/snake-game/static/js/runtime~main.9b2c6772.js"
+  }
+]);
